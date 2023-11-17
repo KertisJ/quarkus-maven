1cd42fc3844eb4040081550cf5b9404d3c16b3c1
KertisJ <jaspersmouafo@gmail.com>
2023-11-17 09:53:10 +0100

    JM: Add of pipeline

---
+++ src/main/java/org/gs/test/quarkusmaven/HelloQuarkus.java
@@ -0,0 +1,18 @@
+package org.gs.test.quarkusmaven;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloQuarkus {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello World!";
+    }
+}
+
+
---
+++ src/test/java/org/gs/test/quarkusmaven/HelloQuarkusIT.java
@@ -0,0 +1,8 @@
+package org.gs.test.quarkusmaven;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class HelloQuarkusIT extends HelloQuarkusTest {
+    // Execute the same tests but in packaged mode.
+}
